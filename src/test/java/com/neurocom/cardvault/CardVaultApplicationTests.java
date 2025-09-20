2bdab24f53712a97e3b61263a86810768fad8c66
z5511846 <z5511846@ad.unsw.edu.au>
2025-09-20 20:25:49 +1000

    Initial Spring Boot project setup

---
+++ src/test/java/com/neurocom/cardvault/CardVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neurocom.cardvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
